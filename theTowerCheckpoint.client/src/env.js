e6e840d1ac921110f3775b606f0fe71922f7a977
Brandon Riggs <aceriggs7@gmail.com>
2023-07-25 17:04:02 -0600

    towerCheckpoint

---
+++ theTowerCheckpoint.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-hiudskyh1mqvazvf.us.auth0.com'
+export const clientId = 'eDVo3e83x1z2XxDmcvh99fZSMGZ6s76b'
+export const audience = 'classroom.com'
